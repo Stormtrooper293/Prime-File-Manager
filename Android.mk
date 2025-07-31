e9b9342eaf0979b025cf374d0cb170df245040d5
Stormtrooper293 <robertjnash21@gmail.com>
2025-07-31 05:07:27 -0500

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,29 +1,83 @@
-#
-# Copyright (C) 2008 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
+// build.gradle.kts
 
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(call all-java-files-under)
+// Apply the plugin for building an Android application.
+plugins {
+    id("com.android.application")
+    // If you're using Kotlin for your source code, add the Kotlin plugin.
+    // id("org.jetbrains.kotlin.android")
+}
 
-LOCAL_PACKAGE_NAME := ghostcommander
-LOCAL_SDK_VERSION := current
+android {
+    // The namespace is required for projects using Android Gradle Plugin 8.0+.
+    // It should match the package name from your AndroidManifest.xml.
+    namespace = "com.primefilemanger"
+    compileSdk = 34 // Target the latest stable SDK, Android 14.
 
-LOCAL_CERTIFICATE := platform
+    defaultConfig {
+        applicationId = "com.primefilemanger"
+        minSdk = 21       // Corresponds to Android 5.0 (Lollipop).
+        targetSdk = 34    // Must match compileSdk for best practices.
+        versionCode = 306 // From your AndroidManifest.xml.
+        versionName = "1.53.6b2" // From your AndroidManifest.xml.
 
-include $(BUILD_PACKAGE)
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
 
+    // Signing configurations for building a standalone app.
+    // The keystore information should be securely stored (e.g., in gradle.properties)
+    // and not hardcoded here.
+    signingConfigs {
+        create("release") {
+            // This block would replace the `LOCAL_CERTIFICATE := platform` for standalone builds.
+            // You would need to generate your own upload key.
+            // storeFile = file("path/to/your/keystore.jks")
+            // storePassword = System.getenv("KEYSTORE_PASSWORD")
+            // keyAlias = System.getenv("KEY_ALIAS")
+            // keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
 
+    buildTypes {
+        release {
+            // Enables code shrinking, obfuscation, and optimization for the release build.
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            // Assign the signing configuration to the release build type.
+            signingConfig = signingConfigs.getByName("release")
+        }
+        debug {
+            // Debug builds are signed with a debug key by default.
+            isMinifyEnabled = false
+        }
+    }
+
+    // Configure Java version compatibility.
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    
+    // For Kotlin projects, you would configure Kotlin options here.
+    // kotlinOptions {
+    //     jvmTarget = "1.8"
+    // }
+
+    // Explicitly define source sets instead of using `all-java-files-under`.
+    // This gives you better control. The default location is `src/main/java`.
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/java") // Default path
+        }
+    }
+}
+
+// Dependencies for the app are declared here.
+// This is the modern way to include libraries like AppCompat, Material Design, etc.
+dependencies {
+    // Example: implementation("androidx.appcompat:appcompat:1.6.1")
+    // Example: implementation("com.google.android.material:material:1.10.0")
+}
